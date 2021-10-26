a58cd0ca201c0a05da9c9d71311d9a40efd8c30f
[aviv polak] <[avivp0437@gmail.com]>
2021-10-26 15:52:46 +0300

    a

--- pokemon-api/src/middleware/userHandler.js
+++ pokemon-api/src/middleware/userHandler.js
@@ -1,14 +1,14 @@
 const { ValidationError } = require("./errorHandler");
 
 const userHandler = (user) => {
-  return (req, res, next) => {
-    if (req.get("username")) {
-      user.name = req.get("username");
-      next();
-    } else {
-      throw new ValidationError("no username header");
-    }
-  };
+    return (req, res, next) => {
+        if (req.get("username")) {
+            user.name = req.get("username");
+            next();
+        } else {
+            throw new ValidationError("no username header");
+        }
+    };
 };
 
 module.exports = userHandler;
